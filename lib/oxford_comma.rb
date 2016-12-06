fa506b6988c22efb2d8c6cb0af229c3ef683032e
Jake Holmes <jakewest@live.ca>
2016-12-06 02:49:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array_length = array.length
+  if (array_length == 1)
+    return array.join()
+  elsif (array_length > 2)
+    return array[0...array.length - 1].join(", ") << ", and " + array[-1]
+  else
+    return array.join(" and ");
+  end
 end
+
+test_array = ["a", "b"]
+puts oxford_comma(test_array)
