1d199f47962bf49ef0d7041f842a5db9abf546c2
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-12-27 19:25:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length < 3
+        array.join(' and ')
+    else
+        last = array.pop
+        array.join(', ') << ', and ' << last
+  end
 end
