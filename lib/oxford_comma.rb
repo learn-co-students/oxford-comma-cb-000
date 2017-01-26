9afe23c789be8c488e7afe92ecd8e2a072fcb96c
ibrahim abdullah <ibrahim.a.abdullah@gmail.com>
2017-01-26 20:07:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
-
+def oxford_comma(jim)
+  if jim.length == 1
+    jim.join
+  elsif jim.length == 2
+    jim.join(" and ")
+  else
+    jim[0..-2].join(", ") + ", and " + jim[-1]
+  end
 end
