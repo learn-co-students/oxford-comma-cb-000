a034400507a1b564b615864870d3c753b293b38b
Salvatore McCarty <salvatore.mccarty@gmail.com>
2017-01-29 19:45:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    array[-1] = "and " + array.last
+    result = array.join(', ')
+    return result
+  end
 end
