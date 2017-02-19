1ed682819aa66253faaeb9688cba73bad81c00ec
John Hillegass <cyrodil@Dragonborn.local>
2017-02-18 23:14:53 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      str = ''
+      last = array.pop
+      array.each { |element| str << "#{element}, "}
+      str += "and #{last}"
+      str
+  end
 end
