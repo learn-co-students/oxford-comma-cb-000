3e91c0758f71f37f0e801dc9771c7400e19dcdeb
Kevin Tran <kevindutran@gmail.com>
2017-02-23 17:47:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+  elsif array.count == 2
+    return array.join(" and ")
+  elsif array.count >= 3
+    array[-1].to_s.prepend("and ")
+    return array.join(", ")
+  end
 end
