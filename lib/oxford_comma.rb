ba421a7c718d2598188b268be2ac57c5924b7c01
James Wilhelm <hello@jamesaverywilhelm.com>
2017-02-27 22:32:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.empty?
+    array.join
+  elsif array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_item = array[-1]
+    array.pop
+    array.join(", ") + ", and " + last_item
+  end
 end
