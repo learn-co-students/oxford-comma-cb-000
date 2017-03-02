8323321ab825e1c07843a5a36afedb65cb2c750b
Brian Johnson <overclocker303@gmail.com>
2017-03-02 21:44:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.length == 1
+    string << array[0]
+  elsif array.length == 2
+    string << array[0]
+    string << " and "
+    string << array[1]
+  else
+    first_number = array.length - 1
+    first_number.times do |n|
+      string << array.shift
+      string << ", "
+    end
+    string << + "and #{array[0]}"
+  end
+  return string
 end
