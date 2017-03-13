7cb7ffef36f29ddc912d57aa804001cfcde7a95a
youssefmys <mohamed.saleh@vet.svu.edu.eg>
2017-03-13 23:14:44 +0200

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.count == 1
+    return array.first
+  elsif array.count == 2
+    return array.join(" and ")
+  else
+    last = array.pop
+    array << "and #{last}"
+    array.join (", ") 
+  end  
+
 end
