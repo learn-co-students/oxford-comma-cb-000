17fda748106899fe3e128645a8eee4797648ea46
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-03-22 14:35:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  case array.count
+  when 1
+    array.join(",")
+  when 2
+    array.join(" and ")
+  else
+    return "#{array[0...-1].join(", ")}, and #{array.last}"
+  end
 end
