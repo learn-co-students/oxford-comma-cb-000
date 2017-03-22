b8df9109ea2fbc4480ea89818fc2c0bd55221e4a
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-03-22 22:24:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+  if (array.count === 1)
+    return array.join()
+
+  elsif (array.count === 2)
+    return array.join(" and ")
+
+  else
+    array_before_last = array[0..-2]
+    string = array_before_last.join(", ")
+    string += ", and #{array[-1]}"
+    return string
+  end
+
 end
