ef0dc895fffc748e03854408ecd04fd47baaa64e
Brian Navaluna <just.brian.en@mail.com>
2017-03-26 21:31:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  str_msg = ""
+  if array.length == 1
+    str_msg = array.first
+  elsif array.length == 2
+    str_msg = "#{array.first} and #{array.last}"
+  else
+    array.each do |word|
+      if word == array.last
+        str_msg << "and "
+        str_msg << word
+      else
+        str_msg << word
+        str_msg << ", "
+      end
+    end
+  end
+  return str_msg
 end
