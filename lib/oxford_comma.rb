318bb8dc33547a5bf128c904282693df90649cc1
Aaron Kelton <aaron.k.kelton@gmail.com>
2017-04-08 22:49:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  res = ''
+  if array.size == 1
+    return array.join()
+  elsif array.size == 2
+    return array.join(' and ')
+  elsif array.size >= 3
+    array.each do |word|
+      if word != array.last
+        res << "#{word}, "
+      else
+        res << "and #{word}"
+      end
+    end
+    return res
+  end
 end
