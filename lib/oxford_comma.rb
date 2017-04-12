69cd7f254822d90beedf1a9b3b815de07c7a5f7e
Dan M <arconk@gmail.com>
2017-04-12 06:03:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  elsif array.length > 1
+    array.join(" and ")
+  elsif array.length == 1
+    array.join
+  else "No array given"
+  end
 end
