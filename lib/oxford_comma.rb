27c52f0d2c2980a4c622f94832fa5a089420ecc4
Alyssa Hogan <alyssabhogan@gmail.com>
2017-04-17 13:37:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elsif array.length > 2
     # insert and at the beginning of the last element
-    array[-1].insert(0, "and ")
+    array[-1].insert(0, 'and ')
   end
   array.join(", ")
 end
