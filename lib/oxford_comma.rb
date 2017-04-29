feeec2875114c6a8b138c36d208f80507464a1dc
Steven Moody <steven@beachhead.io>
2017-04-29 19:53:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.count == 1
+    str = array[0]
+  elsif array.count == 2
+    str = array[0] + " and " + array[1]
 
+  else
+    last_str = array.last
+    array.pop
+    str = array.join(", ")
+    str << ", and #{last_str}"
+  end
+  str
 end
