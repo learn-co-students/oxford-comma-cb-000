84d4adc7905c8e648b1af97417f5af60bb60952e
Jorge Cossi <jorge_cossi@yahoo.com>
2017-05-04 19:16:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  case array.size
+  when 1
+    return array.join
+  when 2
+    array.insert(1, "and")
+    return array.join(" ")
+  else
+    var1 = "and "
+    var1 += array[array.size-1]
+    array[array.size-1] = var1
+    return array.join(", ")
+  end
 
 end
