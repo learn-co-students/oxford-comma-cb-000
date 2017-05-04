2925fc2f88eeff34c3ac6581f07aa3958fdb4ada
Devan Moylan <dmmoylan@gmail.com>
2017-05-04 02:18:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length > 2
+    stringify_array = array.join(", ")
+    comma_index = stringify_array.rindex(",") + 1
+    stringify_array.insert(comma_index, ' and')
+  end
 end
