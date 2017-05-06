ea6fc66774330691f52d187399e252674f3a6b5b
Marcus Havanek <marcus.havranek@havranek.co.at>
2017-05-06 10:06:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last_elem = array.pop
+    joined = array.join(", ")
+    complete = joined + ", and " + last_elem
+  end
 end
