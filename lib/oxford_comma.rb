2b23474968835a7b72e54ff0dfae4f807d448599
Alya Boumiza <boumizaalya@gmail.com>
2017-05-08 02:48:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.index(array.last) == 1
+  array.join(" and ")
+elsif array.index(array.last) == 0
+  array.join
+elsif array.index(array.last) == 2
+array[array.index(array.last)] = "and " + array[array.index(array.last)]
+array.join(", ")
+elsif array.index(array.last) > 2
+array[array.index(array.last)] = "and " + array[array.index(array.last)]
+array.join(", ")
 
+
+  end
 end
