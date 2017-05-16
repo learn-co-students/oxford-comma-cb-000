c94502f4f93a3afc99be11a155084b3f26d9df53
Robert Downey <countrysc@yahoo.com>
2017-05-16 00:26:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  elements = array.length
+  if elements == 1
+    array.join
+  elsif elements == 2
+    array.join(" and ")
+  elsif elements >= 3
+    insert = array.last.length + 1
+    insert2 = -insert
+    array = array.join(", ")
+    array.insert(insert2, "and ")
+  end
 end
