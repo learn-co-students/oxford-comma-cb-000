9bfb475b6fee703c09f57bfedd69dec11e41a27f
Uzor <uubabukoh@gmail.com>
2017-06-05 13:21:09 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  result = ""
+  case array.size
+  when 0
+    result = ""
+  when 1
+    result = array.first
+  when 2
+    result = "#{array.first} and #{array.last}"
+  when 3
+    result = "#{array.first}, #{array[1]}, and #{array.last}"
+  else
+    arr_string = array.slice(0,array.size-1).join(", ")
+    result = "#{arr_string}, and #{array.last}"
+  end
+return result
 end
