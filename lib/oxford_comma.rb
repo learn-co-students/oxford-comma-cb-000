30b442b77b88341877c0baef7f764a6d957ba1bf
Dan Blondell <danblondell@gmail.com>
2017-07-03 21:59:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_sentence = array[0..array.length-2].join(", ")
+    new_sentence << ", and #{array[-1]}"
+  end
 end
