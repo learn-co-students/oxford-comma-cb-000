2323109d13a43b30db7e872045474f1dbee5ceb9
nikifi <nikifiona@gmail.com>
2017-07-05 01:12:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+
+	if array.count > 2
+		last = array.pop
+	    myString = array.join(", ")		
+	    myString << ", and #{last}" 
+	 elsif array.count == 2
+	 	last = array.pop
+	 	myString = array.join(", ")
+	    myString << " and #{last}"
+	 else
+	 	array.join
+	 end 
+
+
 end
+
