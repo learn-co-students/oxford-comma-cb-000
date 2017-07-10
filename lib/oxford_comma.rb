9931e9929805b361f19e9976b31d314d4dc5e2ac
Ben Folk-Sullivan <benfolksullivan@gmail.com>
2017-07-10 02:26:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = ", and #{array.pop}"
+    list = array.join(", ")
+    list << last
+  end
 end
