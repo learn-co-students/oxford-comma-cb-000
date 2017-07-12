0e016d9fa0cd415a200501368470428129386cf5
Benjamin Paar <bpaar1@gmail.com>
2017-07-12 03:31:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+  size = array.size
+  index = size - 1
+    if size > 1
+      array.insert(array.size-1, "and ")
+    end
 
+    while index > 0
+      array[index].insert(0, " ")
+      index -= 1
+    end
+
+    if size > 2
+      index = 0
+      while index <= size - 2
+        array[index] << ","
+        index += 1
+      end
+    end
+    array.join
 end
