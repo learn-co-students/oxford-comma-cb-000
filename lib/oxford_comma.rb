72dc35836c9dbe9212a9334a12c40de77b30d0a9
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-07-23 05:12:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    result_at_1 = array.join
+    return result_at_1
+elsif array.length == 2
+    result_at_2 = array.join(" and ")
+    return result_at_2
+  elsif array.length == 3
+    last = array.pop
+    result = array.join(", ")
+    last = ", and " + last
+    result = result + last
+elsif array.length > 3
+  last = array.pop
+  result = array.join(", ")
+  last = ", and " + last
+  result = result + last
+  return result
+end
 end
