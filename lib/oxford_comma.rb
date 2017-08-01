ad674fc78eff00e5e515fce7dd1c270cf05f0019
Michael Palmer <lawlux@gmail.com>
2017-08-01 16:20:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join( " and " )
+elsif array.length == 3
+      #array.insert(2, "and")
+  #array.join(" , ")
+  array[0..1].join(", ") + ", and " + array[2]
+elsif array.length > 3
+  last_element = array.pop
+  array.join(", ") + ", and " + last_element
+end
 end
