b3d8d49591879826609d1c37cf3f3d634189e788
Shawnda Fortenbaugh <shawndacfortenbaugh@gmail.com>
2017-08-05 18:42:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  elsif array.length >= 3
-    x = array.pop
-    array.join
-    array.push("and #{x}")
-    array.join(", ")
-
+  if array.length == 1 #if the array consists of one element
+    array.join #turn it into a string
+  elsif array.length == 2 #if the array consists of two elements
+    array.join(" and ") #join string together with "and"
+  elsif array.length >= 3 #if the array consists of three or more elements
+    x = array.pop #remove the last element and stores it [a, b, c, d] is now [a, b, c], x = d
+    array.join #turn into a string ["abc"]
+    array.push("and #{x}") #add to the end of the string ["abc", "and x"] x = d
+    array.join(", ") #turn into a string "a, b, c, and x"
   end
 end
