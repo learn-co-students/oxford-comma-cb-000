091e1d8dbfd00242b995d13570e3dc53e1323b94
OneCuteLiz <liz.jenerson@gmail.com>
2017-08-06 17:00:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  case array.length
+    when 1
+      array.join(", ")
+    when 2
+      array.join(" and ")
+    else
+      last_element = array.pop
+      array.join(", ") << ", and #{last_element}"
+  end
 end
