aaa4e9dd0371e0e8e44ed8a747f3772a21345e0f
Ashley Ryan <ashleyskyeryan@gmail.com>
2017-08-11 21:21:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+else
+  string_1 = array[0..-2].join(", ")
+  string_2 = array[-1].to_s
+  string = "#{string_1}, and #{string_2}"
+  return string
+end
 end
