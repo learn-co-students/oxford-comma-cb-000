31c2c6b3ef67f1140d083b2e04368fb63105d3d7
Kunpeng Liu <frozenwind85@gmail.com>
2017-08-17 19:53:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+  convert = ""
+  array_front = array
+  array_last = array_front.pop
+  convert = array_front.join(", ")
+  convert << ", and #{array_last}"
 end
