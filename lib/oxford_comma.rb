d4c0bacc2a2fdfcdd27c6c1434729c695d476d70
Michael Wahlin <wahlin@gmail.com>
2017-09-30 11:17:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(' and ')
+  else
+    array_size = array.size
+    tail_str = array[-1]
+    head_str = array[0,array_size - 1].join(', ')
+    head_str + ', and ' + tail_str
+  end
 end
