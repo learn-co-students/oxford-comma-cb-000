7b593f1dfacacf573ba38d8ad7dbe5169e7ed86f
Marina Paskal <paskalm@rogers.com>
2017-10-05 03:03:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last = array.pop
+    result = array.join(", ")
+    last = ", and " + last
+    result + last
+  end
 end
