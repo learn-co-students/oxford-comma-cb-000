c6495f59cd9b5b4614a3a91634a7044d747b8f7a
Ni Chia <nichia@gmail.com>
2017-10-11 20:20:51 +0000

    manipulate arrays and strings

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  #array_string = array.join(", ")
+  array_string=""
+  if array.size == 1
+    array_string = array[0]
+  elsif array.size == 2
+    array_string = array.join(" and ")
+  else
+    array_string = array[0, array.size - 1].join(", ") + ", and " + array[array.size - 1]
+  end
+  #binding.pry
+  return array_string
 end
+
+#array=["fiddleheads","okra","peter","kohlrabi"]
+#oxford_comma(array)
