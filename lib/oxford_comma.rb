eaeb6908f7ca20492f0b2feecc03593b93785147
Micah Ortega <classytattoo@hotmail.com>
2017-10-22 21:23:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.length == 1
+ joinedarr = array.join
+    return joinedarr
+elsif array.length == 2
+ array.join(" and ")
+ else
+   array.push[-1] = "and #{array[-1]}"
+   array.join(", ")
+end
+
+
 
 end
