ac2db356074cbe0ad763c9e49ef7562ed818af7e
Heather Christman <efl7a@yahoo.de>
2017-11-09 01:33:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-=begin
+
 def oxford_comma(array)
   if array.size == 1
     array[0]
@@ -7,13 +7,4 @@ def oxford_comma(array)
     elsif array.size >= 3
       "#{array.slice(0, (array.size-1)).join(", ")}, and #{array.last}"
   end
-end
-=end
-def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")
 end
