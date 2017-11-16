17b046e36c1c84149dbd89c1ce5b11e4768f6f52
Chris Nunez <cenunez47@gmail.com>
2017-11-16 18:55:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,13 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
+  elsif array.length == 2
     x = array.join(", ")
     y = x.split
     z = y.insert(-2, "and")
     z.join(" ")
   else
     array[0..-2].join(", ") + ", and " + array[-1]
+    #[0..-2] the -2 selects the second-to-last element of the array(.join inserts the comma in that spot)
   end
 end
