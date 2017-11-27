9f25268aa342a1a1dc5f998cc8bfd42ca8a78097
Jawann <jawann@gmail.com>
2017-11-27 02:18:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+  return "#{array.first}" if array.size == 1
+  return "#{array.first} and #{array.last}" if array.size == 2
+  holder = array[0...-1].join(", ")
+  holder << ", and #{array.last}"
 end
