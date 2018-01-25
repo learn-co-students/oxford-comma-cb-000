b15e8865a3623e52303a1bd52aa44286d48d353e
Alyssa Foxx <alyssamariefoxx@gmail.com>
2018-01-25 02:29:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+#Write a method `oxford_comma` that takes an argument array of string elements and converts it into a string using the Oxford comma.
+#For example, the array `["fiddleheads","okra","kohlrabi"]` should get converted to the string `"fiddleheads, okra, and kohlrabi"`.
 def oxford_comma(array)
-
+  if array.count == 1
+      array.join
+  elsif array.count == 2
+      array.join(" and ")
+  else #array.count > 3
+      array[-1].insert(0, "and ")
+      array.join(", ")
+  end
 end
