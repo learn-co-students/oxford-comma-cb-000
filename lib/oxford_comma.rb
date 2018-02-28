f953923caff68d40bf5266aa21ed5c466ecb064b
adam n <adam.nawroc@gmail.com>
2018-02-28 16:18:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+  string = ''
+  array.each_with_index do |item, i|
+
+  if array.size == 1
+    string += item
+  elsif array.size == 2 and i + 1 != array.size
+    string += "#{item} "
+  elsif i + 1 == array.size
+    string += "and #{item}"
+  else
+    string += "#{item}, "
+  end
+
+  end
+
+  string
 end
