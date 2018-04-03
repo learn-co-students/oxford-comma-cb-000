4dd59c3c0f741591848bde165c1acb2133d78aed
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-03 17:08:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.count
+  when 1
+    array[0]
+  when 2
+    array[0] + " and " + array[1]
+  else
+    array << "and " + array.pop
+    array.join(", ")
+  end
 end
