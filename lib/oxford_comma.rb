95ddf47101415ad670f17b7828d55ab2058373b7
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-04-05 03:26:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+   fiddleheads = array.join
+      return fiddleheads
+  elsif array.length == 2
+   array.join(" and ")
+   else
+     array.push[-1] = "and #{array[-1]}"
+     array.join(", ")
+  end
 end
