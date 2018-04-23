d136ff27f18ac175e1cc336c8e125e06adf50987
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-23 17:44:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  remove = array.length - 2
+  if array.length <= 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_array = array.flat_map { |x| [x, ", "]}.tap(&:pop)
+    new_array.insert(-2, "and ")
+    new_array.join()
 
+  end
 end
