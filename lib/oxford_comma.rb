0e2702bb71b5d3ea69c2938f9f70a12d1bf84ee6
Elvis Santana <casualelvis@gmail.com>
2018-04-25 01:02:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  a = []
+  array.each do |blah|
+    a << blah
+  end
 
+  if a.length <= 1
+    a.join
+  elsif a.length == 2
+    a.join(" and ")
+  else
+    a.insert(-1, "and " + a.pop)
+    a.join(", ")
+  end
 end
