b8c187ce854db9a380c3563c356c47540e04505b
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-05-17 11:11:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return array.join(" and ")
+  when 2..100
+    x = "and "
+    x << array.pop
+    array << x
+    return array.join(", ")
+  end
 end
