2111c8ce30247c468f388e846bc70777ee5c2d1a
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-21 01:35:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  new_array = array
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    new_array.join(", ")  
+#    array += array.map! { |i|  i + ", " }
+#      new_array << i + ", "
+#    end
+#    new_array.insert(-2, " and ")
+#    new_array.join
+  end
 end
