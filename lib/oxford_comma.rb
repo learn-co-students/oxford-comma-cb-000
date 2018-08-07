6b075a051fa3bf509e13fdcdd396c73e7e323f5a
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-08-07 04:55:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+ result = "";
+  if array.size == 1
+    result = "#{array[0]}"
+  elsif array.size == 2
+    result = array.join(" and ")
+  else
+    result = array[0..-2].join(", ")
+    result << ", and #{array[-1]}"
+  end
+  result
 end
+
+ 
