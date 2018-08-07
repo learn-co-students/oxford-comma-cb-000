782b88f16ee02f0c85b566390b4a1882fb0cc93c
Mitesh Patel <mit06@hotmail.com>
2018-08-07 17:40:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_arr = []
+    array.insert(-2, "and")
+    new_arr << array[0..-2].join(", ")
+    new_arr << " " + array[-1]
+    new_arr.join
+  end
 end
+
+puts oxford_comma(["okra", "beans", "doigs"])
+
+#
