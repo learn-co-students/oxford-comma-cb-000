d1c52562b6f62f8d7865995b120fc595f84ac2ac
Mark Lindberg <mentalmegalodon@gmail.com>
2018-08-11 21:11:23 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array[0] + " and " + array[1]
+  else
+    array[array.size - 1] = "and " + array.last
+    return array.join(", ")
+  end
 end
