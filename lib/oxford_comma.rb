86809ddf7c662eabfd2cdb3f6d621070d8ecfc62
Jason Viger <jason.viger@gmail.com>
2018-08-13 11:00:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+ 
+ if array.length == 0 
+   return array
+else 
+  x = 0
+    while x < array.length -1
+  
+      array[x] << ", "
+      x += 1 
+    
+    end
+    
+    array[array.length - 1] = "and ".join(array[array.length - 1])
+end
+ 
+return array
+ 
 end
