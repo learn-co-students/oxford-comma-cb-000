c6adb9a0243bbc9e9a3c7b9713e6b73d7a6b8603
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-23 04:25:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    string = array[0]
+    return string
+  elsif array.length == 2
+    string = array[0] + " and " + array[1]
+  else
+  last_item = array.last
+  array.pop
+  string = array.join(", ")
+  string = string + ", and " + last_item
+  return string
+end
 end
