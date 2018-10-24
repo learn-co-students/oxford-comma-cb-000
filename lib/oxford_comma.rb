c63b3fa4b17b81f4386892c7d170923ead3b548f
Tjisana Kerr <tjisana@gmail.com>
2018-10-24 18:51:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+    when 1
+      return array[0]
+    when 2
+      return "#{array[0]} and #{array[1]}"
+    else
+      temp = array.join(", ")
+      temp[temp.rindex(",")] = ", and"
+      return temp
+  end
 end
