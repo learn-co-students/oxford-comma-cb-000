328ecb83f47f72a3375eeed989e1a17049568854
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-07 15:51:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+if array.length ==1
+array[0]
+
+elsif array.length==2
+array[0..1].join(" and ")
+
+else
+array.insert(-2,"and #{array[-1]}").pop
+array.join(", ")
+
+end
 end
