79db37286afc7cdbb19a71387b5a69c0cc64d9a6
Adam Mrachek <amrachek@outlook.com>
2019-07-05 23:20:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  string = ""
+  array.each do |element|
+    if element == array.first
+      string << "#{element}"
+    elsif array.size == 2 && element == array.last
+      string << " and #{element}"
+    elsif element == array.last
+      string << ", and #{element}"
+    else
+      string << ", #{element}"
+    end
+  end
+  string
 end
